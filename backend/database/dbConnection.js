d318e1f4da62554e07844786160b5228de8dfe98
GauravSimkar <kvkgh21@gmail.com>
2025-01-13 01:31:39 +0530

    mongoDB Atlas

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName:"MERN_STACK_JOB_SEEKING"
-    }).then(()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("Connected to database")
     }).catch((err)=>{
         console.log(`Some error occurred while connecting to database: ${err}`)
