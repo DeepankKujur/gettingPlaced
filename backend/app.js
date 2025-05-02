c12ac5ceac00c28d83c7069950bcfaa2270b9c38
NITianHarsh <harshitverma25304@gmail.com>
2025-05-03 00:38:02 +0530

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,7 @@ dotenv.config();
 const app = express();
 dbConnection();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, // Your frontend origin
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
